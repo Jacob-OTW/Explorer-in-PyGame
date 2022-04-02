0873848e3028947e57251f5d0326da4bc99b248c
Jacob-OTW <erdnussflipshow@gmail.com>
2022-04-02 23:51:43 +0200

    Optimized missile, will now use proper line of sight

--- missile_obj.py
+++ missile_obj.py
@@ -4,11 +4,13 @@ from settings import stage
 from player_obj import play
 from planet_obj import planet_group
 from radar_obj import radar
+from nav_objs import Mimic, mimic_group
 
 
 def add_missile():
     missile_group.add(
-        Missile((stage.XScroll + play.rect.centerx, stage.YScroll + play.rect.centery), (play.XF, play.YF)))
+        Missile((stage.XScroll + play.rect.centerx, stage.YScroll + play.rect.centery)))
+    mimic_group.add(Mimic(missile_group.sprites()[-1]))
 
 
 def dis_to(mp, tp):
@@ -30,21 +32,21 @@ def dir_to(mp, tp):
 
 
 class Missile(pygame.sprite.Sprite):
-    def __init__(self, start, force):
+    def __init__(self, start):
         super().__init__()
         self.idle = pygame.transform.scale(pygame.image.load('Assets/Missile/idle.png').convert_alpha(), (80, 80))
         self.image = self.idle
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.start = start
-        self.force = pygame.math.Vector2(force)
         self.angle = 0
-        self.health = 900
+        self.health = 1200
         self.target = radar.lock
+        self.seen = True
+        self.speed = 15
 
     def update(self):
         # Position
-        self.start += self.force
         self.image = pygame.transform.rotate(self.idle, self.angle)
         self.rect = self.image.get_rect(center=(self.start[0] - stage.XScroll, self.start[1] - stage.YScroll))
 
@@ -56,12 +58,12 @@ class Missile(pygame.sprite.Sprite):
         # Tracking
         if self.target:
             convert = 360 / (math.pi * 2)
-            x = self.target.rect.centerx + ((dis_to(self.rect.center, self.target.rect.center) / abs(self.force[0]) + 2) * self.target.XF / 2)
-            y = self.target.rect.centery + ((dis_to(self.rect.center, self.target.rect.center) / abs(self.force[1]) + 2) * self.target.YF / 2)
+            x = self.target.rect.centerx + ((dis_to(self.rect.center, self.target.rect.center) / self.speed) * self.target.XF)
+            y = self.target.rect.centery + ((dis_to(self.rect.center, self.target.rect.center) / self.speed) * self.target.YF)
             predict = (x, y)
             self.angle = dir_to(self.rect.center, predict) - 90
             self.start = (
-                self.start[0] + (math.cos(self.angle / convert) * 2), self.start[1] - math.sin(self.angle / convert) * 2)
+                self.start[0] + (math.cos(self.angle / convert) * self.speed), self.start[1] - math.sin(self.angle / convert) * self.speed)
 
         # Collision
         for planet in planet_group.sprites():
